d00afb2dfc9c1a9e52a8219f87d98425cffe36f1
ilovemetal12 <jair.alviarez@gmail.com>
2023-09-09 20:12:55 -0600

    NavBar Fixes

--- app/components/NavBar/NavBar.jsx
+++ app/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   AppBar,
   Toolbar,
@@ -12,24 +12,21 @@ import {
   List,
   ListItemText,
   ListItemButton,
-} from "@mui/material";
-import logo from "../../assets/ob_logo.png";
-import logo_text from "../../assets/ob_logo_text.png";
-import SideMenu from "./DrawerMenu";
-import { Link } from "react-router-dom";
+} from '@mui/material';
+import logo from '../../assets/ob_logo.png';
+import logo_text from '../../assets/ob_logo_text.png';
+import SideMenu from './DrawerMenu';
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <AppBar
-      component={"nav"}
-      className="AppBar"
-    >
+    <AppBar component={'nav'} className="AppBar">
       <Toolbar className="ToolBar">
         <Stack spacing={1} direction="row" className="leftContainer">
           <Stack
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
           >
             <SideMenu />
           </Stack>
@@ -42,29 +39,29 @@ const NavBar = () => {
             </Link>
           </Stack>
         </Stack>
-        <Stack direction="row" spacing={2} className="rightContainer">
+        <Stack direction="row" className="rightContainer">
           <List className="list">
-            <ListItemButton>
+            <ListItemButton sx={{justifyContent: 'center'}}>
               <Link to="/" className="NavLink">
                 <Typography>Inicio</Typography>
               </Link>
             </ListItemButton>
-            <ListItemButton>
+            <ListItemButton sx={{justifyContent: 'center'}}>
               <Link to="/services" className="NavLink">
                 <Typography>Servicios</Typography>
               </Link>
             </ListItemButton>
-            <ListItemButton>
+            <ListItemButton sx={{justifyContent: 'center'}}>
               <Link to="/store" className="NavLink">
                 <Typography>Tienda</Typography>
               </Link>
             </ListItemButton>
-            <ListItemButton>
+            <ListItemButton sx={{justifyContent: 'center'}}>
               <Link to="/about" className="NavLink">
                 <Typography>Conócenos</Typography>
               </Link>
             </ListItemButton>
-            <ListItemButton>
+            <ListItemButton sx={{justifyContent: 'center'}}>
               <Link to="/contact" className="NavLink">
                 <Typography>Contacto</Typography>
               </Link>
